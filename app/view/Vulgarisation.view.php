7bea0ed41ad505eeb4633108291c9a4cca341a94
Guilherme0424 <144895181+Guilherme0424@users.noreply.github.com>
2024-05-22 15:33:51 +0200

    Update Vulgarisation.view.php

--- app/view/Vulgarisation.view.php
+++ app/view/Vulgarisation.view.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vulgarisation</title>
-    <link rel="stylesheet" href="Vulgarisation.css">
+    <link rel="stylesheet" href="public\css\Vulgarisation.css">
 </head>
 
 <body>
