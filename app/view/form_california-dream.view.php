586909fcadd2b827b18ee00725f8c51f21d542cc
Guilherme0424 <144895181+Guilherme0424@users.noreply.github.com>
2024-05-23 17:56:16 +0200

    Add files via upload
    Test

---
+++ app/view/form_california-dream.view.php
@@ -0,0 +1,9 @@
+<form action="ajout_biere.php" method="post" enctype="multipart/formdata">
+    <div>
+        <label for="quantite">Quantité</label>
+        <input type="hidden" name="ref" value="2" >
+        <input type="number" name="" id="">
+        <input type="text" name="quantite" id="quantite">
+    </div>
+    <button type="submit">Ajouter au panier</button>
+</form>
