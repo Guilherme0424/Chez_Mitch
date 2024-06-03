d1537d0aff70638f5eded3b8ff7e3fd8ae345feb
Nathanchl <jeremi.chloe@gmail.com>
2024-06-03 16:48:19 +0200

    aled

--- app/view/Avis.view.php
+++ app/view/Avis.view.php
@@ -1,12 +1,12 @@
 <section>
     <div class="fondblue">
         <h1>AVIS</h1>
-        <div class="bibi">Lol</div>
+        <div class="bibi"><img src="" alt=""></div>
         <h3>Ajouter un avis : </h3>
         <div></div>
         <h4>Commentaire :</h4>
         <div></div>
         <div></div>
     </div>
-    <div class="vaguebi"></div>
+    <div class="vaguebi"><img src="public/images/Vague Sable Bicolor.svg" alt=""></div>
 </section>
