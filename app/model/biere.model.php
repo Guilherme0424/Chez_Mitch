55c12105190e261a02d8415065534d0205ef869c
Guilherme0424 <144895181+Guilherme0424@users.noreply.github.com>
2024-06-04 16:39:09 +0200

    Update biere.model.php

--- app/model/biere.model.php
+++ app/model/biere.model.php
@@ -1,5 +1,14 @@
 <?php
-function getSpecificBeers($pdo, $ids){
-    $list = join(",", $ids);  "SELECT * FROM bière WHERE id_bière IN (" . $list . ")"; } 
+<?php
+function getSpecificBeers(PDO $pdo, $ids) {
+    $stmt = $pdo->prepare("SELECT * FROM biere WHERE id_biere=:id");
+    $stmt->bindParam(':id',$ids, PDO::PARAM_INT);
+    $stmt->execute();
+    $produit=$stmt->fetchAll();
+    // while ($row = mysqli_fetch_assoc($result)) {
+    //     $biere[$row['id']] = $row;
+    // }
+    return $produit;
+}
 
 
