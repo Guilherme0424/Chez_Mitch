e4221e7951c59b9e838f3a9aef411e3ab05489cf
Seeymaa <seyma.acar@outlook.fr>
2024-06-05 08:42:48 +0200

    page biere

--- biere.php
+++ biere.php
@@ -1,70 +1,27 @@
-<!DOCTYPE html>
-<html lang="fr">
+<?php
+session_start();
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="public/css/biere.css">
-    <title>L'Originale</title>
-</head>
+try {
+    $pdo = new PDO('mysql:host=localhost;dbname=cdce5547_2024_chez-mitch_1', 'root', '');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (Exception $e) {
+    die('Erreur : ' . $e->getMessage());
+}
 
-<body>
-    <section>
-        <div class="gauche">
-            <div class="titre">
-                <h1>L'ORIGINALE</h1>
-            </div>
-            <div class="texte">
-                <h3>L’originale est une bière belgian pale ale à l’effigie de Mitch.
-                    Elle retrace son parcours en tant qu’incroyable surfeur cali-breton.🌴</h3>
-                    <a href=""><img src="public/images/rond noir.svg" alt="rond noir"></a>
-                    <a href=""><h4 id="achat">Achetez <br>maintenant !</h4></a>
-            </div>
-            <div class="flèche">
-                <img src="public/images/flèche noir.png">
-            </div>
-            <div class="rond">
-                
-            </div>
-        </div>
-        <div class="vaguedroite">
-            <img src="public/images/Vague Verticale beige.svg">
-        </div>
-            <div class="biere">
-            <img src="public/images/Originale.png" id="originale">
-            </div>
-        <div class="droite">
-            <div class="liste">
-                <ul>
-                    <li><h3>Degrés</h3></li>
-                </ul>
-                <ul class="sous-titre">
-                    <li><h4>7.0</h4></li><br>
-                </ul>
+if (!isset($_GET['id']) || !is_numeric($_GET['id'])) {
+    die('Erreur : aucun ID de bière spécifié.');
+}
 
-                <ul>
-                    <li><h3>Ingrédients</h3></li>
-                </ul>
-                <ul class="sous-titre">
-                    <li><h4>Pilsen</h4></li>
-                    <li><h4>Pale Ale</h4></li>
-                    <li><h4>Flocon d'avoine</h4></li>
-                    <li><h4>Carahell</h4></li>
-                    <li><h4>Houblons</h4></li>
-                    <li><h4>Levure</h4></li>
-                    <li><h4>Graines de coriandre</h4></li>
-                    <li><h4>Fleurs de sureau</h4></li>
-                    <li><h4>Fleurs de cannelle</h4></li>
-                    <li><h4>Poivre blanc</h4></li><br>
-                </ul>
-                <ul>
-                    <li><h3>Type</h3></li>
-                </ul>
-                <ul class="sous-titre">
-                    <li><h4>Blonde</h4></li>
-                </ul>
-            </div>
-        </div>
-    </section>
-</body>
-</html>
+$id = (int)$_GET['id'];
+$query = $pdo->prepare('SELECT * FROM biere WHERE id_biere = :id');
+$query->execute(['id' => $id]);
+$biere = $query->fetch();
+
+if (!$biere) {
+    die('Erreur : bière non trouvée.');
+}
+
+$page_title = $biere['Nom'];
+include 'app/view/biere.view.php';
+
+?>
